59d2bcb5995dff1c94c873e7226988abfa4563f5
hoang <huyhoangle789990@gmail.com>
2023-03-17 10:32:20 +0700

    #3

--- src/components/Modals/FriendsModal.js
+++ src/components/Modals/FriendsModal.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Modal, Button, Form, InputGroup } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { editExercise } from "../../actions/courseAction";
 
 const FriendsModal = ({ showModalFriends, setShowModalFriends, isShowModal }) => {
-
+  const [chooseFriends, setChooseFriends] = useState([]);
+  const [showScrollbar, setShowScrollbar] = useState(false);
   const friends = [
     {
       id: 1,
@@ -38,7 +39,55 @@ const FriendsModal = ({ showModalFriends, setShowModalFriends, isShowModal }) =>
     },
     {
       id: 6,
+      name: "Oracssle",
+      avatar: "https://i.pravatar.cc/150?img=6",
+      status: "offline"
+    },
+    {
+      id: 6,
+      name: "Oraaacle",
+      avatar: "https://i.pravatar.cc/150?img=6",
+      status: "offline"
+    },
+    {
+      id: 6,
       name: "Oracle",
+      avatar: "https://i.pravatar.cc/150?img=6",
+      status: "offline"
+    },
+    {
+      id: 6,
+      name: "Orascle",
+      avatar: "https://i.pravatar.cc/150?img=6",
+      status: "offline"
+    },
+    {
+      id: 6,
+      name: "Oraccle",
+      avatar: "https://i.pravatar.cc/150?img=6",
+      status: "offline"
+    },
+    {
+      id: 6,
+      name: "Oraclasdce",
+      avatar: "https://i.pravatar.cc/150?img=6",
+      status: "offline"
+    },
+    {
+      id: 6,
+      name: "Oracladea",
+      avatar: "https://i.pravatar.cc/150?img=6",
+      status: "offline"
+    },
+    {
+      id: 6,
+      name: "Oraasdcles",
+      avatar: "https://i.pravatar.cc/150?img=6",
+      status: "offline"
+    },
+    {
+      id: 6,
+      name: "Oraclaase",
       avatar: "https://i.pravatar.cc/150?img=6",
       status: "offline"
     },
@@ -54,6 +103,17 @@ const FriendsModal = ({ showModalFriends, setShowModalFriends, isShowModal }) =>
     setShowModalFriends(false);
     isShowModal(true)
   }
+
+  const handleChooseFriends = (index) => {
+    if (chooseFriends.includes(index)) {
+      const new_arr = chooseFriends.filter(item => item !== index);
+      setChooseFriends(new_arr)
+    } else {
+      const updatedList = [...chooseFriends, index];
+      setChooseFriends(updatedList);
+    }
+  }
+
 
   return (
     <div>
@@ -76,15 +136,25 @@ const FriendsModal = ({ showModalFriends, setShowModalFriends, isShowModal }) =>
             <span className="lblDone">Xong</span>
           </div>
           <div className="friends-list-container">
-            <div className="suggest">GỢI Ý</div>
-            <div className="friends-list" id="style-1">
-              {friends.map((item) =>
-                <div key={item.id} className="info-friends">
-                  <img src={item.avatar} alt={item.name} />
-                  <span>{item.name}</span>
-                </div>
-              )}
+            {chooseFriends.length > 0 ? (<>
+              <span className="assign-tags">Đã gắn thẻ</span>
+              <div className="border-chooseFriends">
+                {chooseFriends.map((item, index) =>
+                  <div key={index} className="choose-Friend">{item}</div>
+                )}
+              </div></>) : null}
+            <div className="scroll">
+              <div className="suggest">GỢI Ý</div>
+              <div className="friends-list" id="style-1">
+                {friends.map((item) =>
+                  <div key={item.id} className="info-friends" onClick={() => { handleChooseFriends(item.name) }}>
+                    <img src={item.avatar} alt={item.name} />
+                    <span>{item.name}</span>
+                  </div>
+                )}
+              </div>
             </div>
+
           </div>
         </Modal.Body>
       </Modal>
